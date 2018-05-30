b9ec4498056d6ad0041abd7e6cfaa7e5f841a0e8
andres <andres.93@outlook.com>
2018-05-30 18:20:59 +0200

    Reply users

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -86,6 +86,38 @@
               <h5 class="card-title">' . $row->nick_msg . ' dice: ' . $row->titulo . '</h5>
               <p class="card-text">' . $row->cuerpo . '</p>
               <p class="card-text"><small class="text-muted">' .$row->fecha . '</small></p>
+
+              <div class="show_comments">
+                <h4>Comentarios</h4>
+
+                <div class="comment">
+                  <div class="d-flex justify-content-end">
+                    <img src="https://image.flaticon.com/icons/png/512/149/149071.png" alt="avatar">
+                  </div>
+                  <div class="content_reply d-flex flex-row">
+                    <a href="#">#usuaioReply</a>
+                    <p>Lunes a las 10:00</p>
+                  </div>
+                  <div class="text_reply">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, praesentium.</p>
+                  </div>
+                </div>
+
+                <div class="comment">
+                  <div class="d-flex justify-content-end">
+                    <img src="https://image.flaticon.com/icons/png/512/149/149071.png" alt="avatar">
+                  </div>
+                  <div class="content_reply d-flex flex-row">
+                    <a href="#">#usuaioReply2</a>
+                    <p>Martes a las 10:00</p>
+                  </div>
+                  <div class="text_reply">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, praesentium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quod laboriosam, et debitis, suscipit rerum?</p>
+                  </div>
+                </div>
+
+
+              </div>
               <hr/>
                 <a class="btn btn-feisbuk rounded-0 shadow-sm mb-2" data-toggle="collapse" href="#' . $row->id_msg . ' " role="button" aria-expanded="false" aria-controls="' . $row->id_msg . '">
                   Responder
@@ -113,6 +145,8 @@
               <h5 class="card-title">' . $row->nick_msg . ' dice: ' . $row->titulo . '</h5>
               <p class="card-text">' . $row->cuerpo . '</p>
               <p class="card-text"><small class="text-muted">' .$row->fecha . '</small></p>
+              <hr/>
+
               <hr/>
               <a class="btn btn-feisbuk rounded-0 shadow-sm mb-2" data-toggle="collapse" href="#' . $row->id_msg . '" role="button" aria-expanded="false" aria-controls="' . $row->id_msg . '">
                   Responder
